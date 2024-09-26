47e8274abd026c8c96e8052557edcc3a0340a56c
seungwb <seungwb@gmail.com>
2024-09-26 17:53:24 +0900

    test2

--- src/main/java/com/example/calendar/CalendarApplication.java
+++ src/main/java/com/example/calendar/CalendarApplication.java
@@ -10,6 +10,7 @@ public class CalendarApplication {
 
         SpringApplication.run(CalendarApplication.class, args);
 //        System.out.println("hello world");
+        //System.out.println("test");
     }
 
 }
